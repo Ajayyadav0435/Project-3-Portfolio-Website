9b5b72ab1a19487b3b02922336ae58e49a8a057e
ajay yadav <ajjuyadav0435@gmail.com>
2024-09-14 22:52:23 +0530

    level A all files are completed

---
+++ index.js
@@ -0,0 +1,12 @@
+<!-- Add this script before the closing </body> tag in index.html -->
+<script>
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+</script>
